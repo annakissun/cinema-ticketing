0ec1fd123c618d601b4d77af3e9126d72dfa1543
annakissun <125438648+annakissun@users.noreply.github.com>
2025-01-11 16:11:33 +0800

    get code working

--- CINEMATICKETING.cpp
+++ CINEMATICKETING.cpp
@@ -7,34 +7,6 @@
 // Meow meow meow meow
 using namespace std;
 
-int main() {
-	cout << "Welcome to Rinlidana Cinema Booking System!" << endl;
-
-    do {
-	    cout << "Please place your order." << endl;
-
-        int movieCode;
-        float concessionTotal = 0f, ticketTotal = 0f;
-
-        concessionTotal = concessionStand();
-
-        movieCode = movieSelection();
-
-        ticketTotal = ticketOrdering();
-
-        // TODO: Output order totals
-        // Sum all of the price of concessions and tickets and display summary of totals
-        cout << "Your total is: " << concessionTotal + ticketTotal;
-
-        // TODO: Ask user if they want to place another order
-
-    } while (); // Check if user wants to place another order
-    
-	cout << "Thank you for ordering with Rinlidana Cinema Booking System. Goodbye!" << endl;
-
-    return 0;
-}
-
 float concessionStand() {
     // TODO: Ask user for concessions they want to buy. Return total value of all concessions.
 }
@@ -46,3 +18,33 @@ int movieSelection() {
 float ticketOrdering() {
     // TODO: Ask user how many tickets they want to buy and of which type. Return the total price of all tickets.
 }
+
+int main() {
+	cout << "Welcome to Rinlidana Cinema Booking System!" << endl;
+
+    do {
+	    cout << "Please place your order." << endl;
+
+        int movieCode;
+        float concessionTotal = 0.0f, ticketTotal = 0.0f;
+
+        concessionTotal = concessionStand();
+
+        movieCode = movieSelection();
+
+        ticketTotal = ticketOrdering();
+
+        // TODO: Output order totals
+        // Sum all of the price of concessions and tickets and display summary of totals
+        cout << "Your total is: " << concessionTotal + ticketTotal << endl;
+
+        // TODO: Ask user if they want to place another order
+
+    } while (false); // Check if user wants to place another order
+    
+	cout << "Thank you for ordering with Rinlidana Cinema Booking System. Goodbye!" << endl;
+
+    return 0;
+}
+
+
